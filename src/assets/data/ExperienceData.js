b548ff823181d2a88de327e5498b2d9ec75e056f
SurajitPhysics <surajitguin83@gmail.com>
2024-12-07 00:30:26 +0530

    Add files via upload

---
+++ src/assets/data/ExperienceData.js
@@ -0,0 +1,29 @@
+export const ExperienceData = [
+
+
+    {
+        id : 1,
+        title : 'State Aided College Teacher-I',
+        institute : 'AKPC MAHAVIDYALAYA (University of Burdwan)',
+        nature: 'Permanent',
+        duration : '01/01/2020 - Present',
+
+    },
+    {
+        id : 2,
+        title : 'Guest Teacher',
+        institute : 'AKPC MAHAVIDYALAYA (University of Burdwan)',
+        nature: 'Temporary',
+        duration : ' 12/08/2013 - 31/12/2019',
+
+    },
+    {
+        id : 3,
+        title : 'Teacher at Practice teaching in B.Ed.',
+        institute : 'Baranagar Vidyamandir',
+        nature: 'Temporary',
+        duration : '12/01/2015 - 09/02/2015',
+
+    },
+
+]
